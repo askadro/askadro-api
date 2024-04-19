90afcc6f44687d39242b322cb0ef9e9a3fdd1ea2
Hakan KORKMAZ <hakan.hizmet.54.gs@gmail.com>
2024-04-19 23:20:33 +0300

    19.04.2024 23:20
    fixed

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -1,6 +1,6 @@
 import { _IsUnique } from "./unique/is-unique";
 import { _IsUniqueConstraint } from "./unique/is-unique-constraint";
 
-export const IsUnique = _IsUnique;
 
-export const IsUniqueConstraint = _IsUniqueConstraint
+export {_IsUnique as IsUnique}
+export {_IsUniqueConstraint as IsUniqueConstraint}
