dc76e2daed16bc6406290245681d7c6601313b2b
Hakan KORKMAZ <hakan.hizmet.54.gs@gmail.com>
2024-04-22 16:41:41 +0300

    22.04.2024 16:41
    Application is running on

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import helmet from "helmet";
+import helmet from 'helmet';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 import { AppModule } from './app.module';
-import { useContainer } from "class-validator";
+import { useContainer } from 'class-validator';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{
-    snapshot:true,
-    logger:["debug","error","fatal","warn",'verbose',"log"]
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+    logger: ['debug', 'error', 'fatal', 'warn', 'verbose', 'log'],
   });
   app.useGlobalPipes(new ValidationPipe({
-    whitelist:true
-  }))
+    whitelist: true,
+  }));
   const configService = app.get<ConfigService>(ConfigService);
-  const appPort = configService.get("PORT") ?? 5062;
+  const appPort = configService.get('PORT') ?? 5062;
   app.use(helmet());
   app.enableCors();
   app.enableVersioning({
     type: VersioningType.URI,
-    prefix: "v",
+    prefix: 'v',
     // defaultVersion: "1"
   });
   // app.use(passport.initialize());
@@ -35,7 +35,10 @@ async function bootstrap() {
   // });
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
-  
-  await app.listen(appPort);
+
+  await app.listen(appPort, () => {
+    console.log(`Application is running on: http://localhost:${appPort}}`)
+  });
 }
+
 bootstrap();
